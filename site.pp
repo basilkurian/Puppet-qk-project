2d9673ee8ce69fb1b1b90be8a82ee9efb5d95344
Basil Kurian <basil@basilkurian.co.cc>
2011-03-01 21:26:09 +0530

    changed

--- site.pp
+++ site.pp
@@ -4,33 +4,11 @@ import "classes/fe_server.pp"
 import "classes/cache_server.pp"
 
 
-class fe2 inherits fe_server
-{
 
-}
-
-
-node "sq-fe2.quickerala.com"
+node "fe.example.local"
 {
 	include fe_server
 }
 
-node "sq-api2.quickerala.com"
-{
-	include api_server
-}
-
-node "sq-cache2.quickerala.com"
-{
-	include cache_server
-}
-
-node "puppet-client-testing.quickerala.com" 
-{ 
-	#include api_server
-	#include db_server
-	include fe_server
-	#include cache_server
-}
 
 
